d2a3c429f14789c7aa65f21c586ceaf8eb3e6747
aadityajsl4117 <aadityajayswal4117@gmail.com>
2025-12-23 18:50:44 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,16 @@
+public class Number
+{
+public static void main (String[] args)
+{
+Scanner input = new Scanner(System.in);
+int n = input.nextInt();
+if(n % 2== 0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
