1d640853d54422ccd4acbde0a27821c3d373e721
aadityajsl4117 <aadityajayswal4117@gmail.com>
2025-12-24 10:21:39 +0530

    Add files via upload

---
+++ lab practise/Dei.java
@@ -0,0 +1,14 @@
+class Dei
+{
+    public static void main(String[] args)
+    {
+        int i = 0;
+        do
+        {
+ 
+            System.out.println("hello");
+            i++;
+        } while (i <=5);  
+        }
+    }
+
