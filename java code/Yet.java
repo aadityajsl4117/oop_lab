ae907c12a28c9649335bbb731543dfdfcc99b00b
aadityajsl4117 <aadityajayswal4117@gmail.com>
2025-12-28 14:11:33 +0530

    Add files via upload

---
+++ java code/Yet.java
@@ -0,0 +1,18 @@
+class Yet
+{
+    public static void main(String...args)
+{
+    String S1 = "JAVA";
+    String S2 = "Jeva";
+    if (S1 == S2)
+    {
+        System.out.println("reference is equal");
+    }
+   else
+   {
+    System.out.println("reference is unequal");
+
+   }
+}
+}
+
