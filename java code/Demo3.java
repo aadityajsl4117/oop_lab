6fc0f46bba3a1f729e423a7ebc36ad4bf4a988aa
aadityajsl4117 <aadityajayswal4117@gmail.com>
2025-12-30 19:17:35 +0530

    Add files via upload

---
+++ java code/Demo3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Demo3 {
+    public static void main(String[] args)
+    {
+       Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the name:");
+        String name = input.nextLine();
+
+        System.out.println("Enter the age:");
+        int age = input.nextInt();
+
+        System.out.println("Enter the salary:");
+        float salary = input.nextFloat();
+
+        System.out.println("\n--- User Details ---");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+    }
+}
+    
+    
