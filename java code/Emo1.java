5c605162e9f700269772224518fc2aa624445241
aadityajsl4117 <aadityajayswal4117@gmail.com>
2026-01-07 23:06:56 +0530

    Add files via upload

---
+++ java code/Emo1.java
@@ -0,0 +1,31 @@
+public class Emo1 {
+    public static void main(String...args)
+    {
+    
+        int i = 1;
+        while(i<=5)
+        {
+         
+            System.out.println("Happy new yaear 26K");   
+        i++;
+
+    }
+    
+    System.out.println("ENter by using for loop");
+    for (int a =0;a<=5;a++)
+    {
+        System.out.println("Happy new year 2026");
+    }
+
+      System.out.println("for using do while loop");
+      int count = 1;
+    do{
+        System.out.println("Happy new year");
+         count++;
+    }while(count<=5);
+    
+
+
+
+}
+}
