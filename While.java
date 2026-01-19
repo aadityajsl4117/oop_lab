90fb008a98f91b4f2d8f208f9cc6e45e7d2ab346
aadityajsl4117 <aadityajayswal4117@gmail.com>
2026-01-19 21:34:19 +0530

    Add files via upload

---
+++ While.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class While
+{
+public static void main(String[] args)
+{
+int rev = 0;
+System.out.println("enter the number");
+Scanner input = new Scanner(System.in);
+int n = input.nextInt();
+int temp = n;
+while (n != 0)
+{
+int digit = n%10;
+rev = rev*10+digit;
+n = n/10;
+}
+if (temp == rev)
+{
+System.out.println("It is palindrom");
+}
+else
+{
+System.out.println("It is not palindrom");
+}
+}
+}
