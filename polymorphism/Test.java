d53a190375a1a4a6fa65577f4f94730fccff0cd7
Aaditya Jaiswal <aadityajayswal4117@gmail.com>
2026-03-23 22:00:17 +0530

    Add files via upload

---
+++ polymorphism/Test.java
@@ -0,0 +1,19 @@
+
+class Animal {
+    void sound() {
+        System.out.println("Animal sound");
+    }
+}
+
+class Cat extends Animal {
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        Animal a = new Cat();
+        a.sound();
+    }
+}
