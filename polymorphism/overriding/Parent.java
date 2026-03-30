884a44a5d661b6b506fad75124942ada3c4b527a
Aaditya Jaiswal <aadityajayswal4117@gmail.com>
2026-03-30 21:58:31 +0530

    Add files via upload

--- polymorphism/overriding/Parent.java
+++ polymorphism/overriding/Parent.java
@@ -1,1 +1,19 @@
+class Parent1 {
+    void add(int a, int b) {
+        System.out.println("Parent Add: " + (a + b));
+    }
+}
 
+class Child extends Parent1 {
+    @Override
+    void add(int a, int b) {
+        System.out.println("Child Add: " + (a + b + 10));
+    }
+}
+
+public class Parent {
+    public static void main(String[] args) {
+        Parent1 obj = new Child();
+        obj.add(5, 3);
+    }
+}
